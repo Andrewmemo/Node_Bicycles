b705b556e573e573d463b97d27a6b46c0b4897a9
Andrew Buhayov <andrii.buhaiov@nure.ua>
2020-09-11 14:36:28 +0300

    DB Connection

--- startup/db.js
+++ startup/db.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 module.exports = function () {
   mongoose
-    .connect("mongodb://localhost:27017/bicycles", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      "mongodb://bicycles-server:TCyPIRRwTB5x3anHvAAz28Uercmh8WAJ5MUUbxogirDCtb7lJwBsvLXOZ2eUVhwUEApgdHv7WaWZXR8y6nV98Q==@bicycles-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@bicycles-server@",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => console.log("Connected to MongoDB"));
 };
